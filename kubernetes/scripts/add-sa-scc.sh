349f1a6b5e2666e1ff8e431af65dbe5e1388655e
bugbiteme <leon.s.levy@gmail.com>
2025-10-02 15:27:23 -0700

    ocp

--- kubernetes/scripts/add-sa-scc.sh
+++ kubernetes/scripts/add-sa-scc.sh
@@ -1,10 +1,1 @@
-# oc adm policy add-scc-to-user anyuid -z otel-sa -n otel-demo
-# Get all deployment names and set the service account on each one
-for deploy in $(oc get deployment -n otel-demo -o jsonpath='{.items[*].metadata.name}'); do
-    oc set serviceaccount deployment/$deploy otel-sa -n otel-demo
-done
-
-oc set serviceaccount deployment/grafana otel-sa -n otel-demo
-oc set serviceaccount statefulset/opensearch otel-sa -n otel-demo
-
-oc -n otel-demo get deploy -o name | xargs -r -L1 oc -n otel-demo rollout restart
+oc apply -k kubernetes -n otel-demo
